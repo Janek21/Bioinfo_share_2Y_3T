e326d270b0c36610f8ec214c28698932379cf33e
Jan Carreras Boada <jan.carreras@alum.esci.upf.edu>
2024-05-13 00:28:15 +0200

    avançant possicions

---
+++ JanC/Comparative/practical2/cfg_phylogenetics/percentatge.py
@@ -0,0 +1,10 @@
+from Bio import SeqIO
+
+record = list(SeqIO.parse("CO1A2_aln.fa", "fasta"))
+count = 0
+count_gaps = 0
+for rec in record:
+    count += len(rec)
+    count_gaps += rec.count('-')
+
+print(100*count_gaps/count)
